859138732daf23d5316d09cc3d3f38b5e9d84acf
gmatheus66 <gmatheus66@hotmail.com>
2019-09-23 20:24:20 -0300

    Crud em Laravel

---
+++ crud-app/resources/views/base.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Laravel Crud</title>
+	<link rel="stylesheet" href="{{ asset('css/app.cs') }}">
+
+</head>
+<body>
+	<div class="container">
+
+		@yield('main')
+		
+	</div>
+
+	<script src="{{ asset('js/app.js') }}" type="text/js"></script>
+</body>
+</html>
